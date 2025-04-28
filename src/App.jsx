56b4404bf37899041ee8c97a52f143f6bd183117
arunsaradgi <saradgi07@gmail.com>
2025-04-28 12:59:13 +0530

    add and delete comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [comments, setComments] = useState([])
+
+  const [text, setText] = useState('')
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    // console.log(Date.now())
+    setComments([...comments, { comment: text, id: Date.now() }])
+    setText('')
+  }
+  
+  
+  const handleDelete = (comment) => {
+    const new_comments = comments?.filter(ele =>{
+      return ele.id !== comment.id
+    } )
+    setComments(new_comments)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Nested Comment System</h1>
+      <form onSubmit={handleFormSubmit}>
+        <textarea value={text} required placeholder='Write a comment...' onChange={(e) => setText(e.target.value)}></textarea>
+        <input type="submit" />
+      </form>
+
+      {
+        comments.map((ele, i) => (<div key={i}>
+          <h1>{ele.comment}</h1>
+          <button onClick={()=>handleDelete(ele)}>Delete</button>
+        </div>
+        ))
+      }
     </>
   )
 }
